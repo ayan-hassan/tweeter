9db2afb4ab2aa812ce55091ed1813942e4210bba
ayan-hassan <ahassan@hotmail.ca>
2023-04-25 23:04:05 -0400

    add feature where counter red when over limit

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,5 +3,12 @@ $(document).ready(function() {
     let maxTweetLength = 140;
     let currentLength = $(this).val().length;
     $(".counter").text(maxTweetLength - currentLength);
+
+    //changes counter to red when over tweet limit
+    if (currentLength > maxTweetLength) {
+      $(".counter").addClass("overTweetLimit");
+    } else if (currentLength <= maxTweetLength) {
+      $(".counter").removeClass("overTweetLimit");
+    }
   });
 });
